e78b1f934cf4b7394c54c462cb568363334005b9
08070113 <ace.li@tradeshift.com>
2020-06-12 12:39:13 +0800

    build

--- web_src/src/components/Search.vue
+++ web_src/src/components/Search.vue
@@ -35,7 +35,7 @@
           data: formdata,
           success: function (result) {
             console.log(result);//打印服务端返回的数据(调试用)
-            page_id = result.data.menu.pages[0].page_id;
+            let page_id = result.data.menu.pages[0].page_id;
             window.location.href = `http://kubectl.bwtsi.cn/web/#/7?page_id=${page_id}`;
           },
           error: function () {
