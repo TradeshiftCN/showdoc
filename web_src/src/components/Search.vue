74b49f9a2aed1753c84f3e4bb97e3206d63d82f6
08070113 <ace.li@tradeshift.com>
2020-06-12 11:21:24 +0800

    update

--- web_src/src/components/Search.vue
+++ web_src/src/components/Search.vue
@@ -7,7 +7,7 @@
       <div class="searchBox">
         <form id="form" >
           <input type="hidden" name="item_id" value="7">
-          <input type="text" class="searchIpt f14" name="keyword" maxlength="100" autocomplete="off"/>
+          <input type="text" id="keyword" class="searchIpt f14" name="keyword" maxlength="100" autocomplete="off"/>
           <input type="button" class="btn cp" @click="submitForm()" value="TS一下" />
 
         </form>
@@ -22,12 +22,17 @@
 		name: "search",
     methods: {
       submitForm() {
+        let formdata = new FormData();
+        formdata.append('item_id', '7');
+        formdata.append('keyword', $("#keyword").val());
+
         $.ajax({
           type: "POST",
           dataType: "json",
-          contentType: "application/json",
+          processData: false,
+          contentType: false,
           url: "http://kubectl.bwtsi.cn:4999/server/index.php?s=/api/item/info",
-          data: $('#form').serialize(),
+          data: formdata,
           success: function (result) {
             console.log(result);//打印服务端返回的数据(调试用)
             page_id = result.data.menu.pages[0].page_id;
@@ -41,46 +46,7 @@
     },
     mounted() {
       var that = this;
-      $('.uname,.topMenus').hover(function(){
-        $('.topMenus').stop(true,true).fadeIn(200);
-      },function(){
-        $('.topMenus').stop(true,true).delay(500).fadeOut(200);
-      });
 
-      //搜索框自动补全;
-      $('.searchIpt').AutoComplete({
-        'data': ['About Me', '审美网', 'abcdefg', 'One', 'Two', 'Three', 'Four', 'Five', 'Six','smwell.com', 'Seven', 'Eight', 'Nine', 'Ten', 'Eleven', 'Twelve'],
-        'itemHeight': 24,
-        'width': 529
-      }).AutoComplete('show');
-
-      $('.ctnerTab a').click(function(){
-        if(!$(this).hasClass('on')){
-          $('.ctnerTab a').removeClass('on').eq($(this).index()).addClass('on');
-          $('.rtNavs').stop(true,true).hide(200).eq($(this).index()).show(300);
-        }
-      });
-
-      $('.menusWrapper a').click(function(){
-        if(!$(this).hasClass('active')){
-          $('.menusWrapper a').removeClass('active').eq($(this).index()).addClass('active');
-          $('.cbox').stop(true,true).animate({top:318},100).hide().eq($(this).index()).animate({top:0},400).show();
-        }
-      });
-
-      $('.smallPics a').click(function(){
-        if(!$(this).hasClass('active')){
-          $('.smallPics a').removeClass('active').eq($(this).index()).addClass('active');
-          $('.picLink').stop(true,true).removeClass('active').eq($(this).index()).addClass('active');
-        }
-      });
-
-      $('.titleT').hover(function(){
-        if(!$(this).hasClass('on')){
-          $('.titleT').removeClass('on').eq($(this).index()).addClass('on');
-          $('.topicB').stop(true,true).hide().eq($(this).index()).show();
-        }
-      });
     }
 	}
 
